f7d30bc0f9b2910b9f6beda2570f9b691eeaeb79
Данил <danil.arhishin@mail.ru>
2025-06-27 20:44:53 +0300

    Book shop

--- app/models/user.py
+++ app/models/user.py
@@ -11,7 +11,3 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.name}"
-
-class Apras(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
